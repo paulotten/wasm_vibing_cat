240b7cc80715b64a35746b11321574025c059724
Paul Otten <potten@my.bcit.ca>
2020-11-24 19:19:14 -0500

    Read meme text from inputs

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-const { createFFmpeg, fetchFile } = FFmpeg;
+function init() {
+    const { createFFmpeg, fetchFile } = FFmpeg;
 
-const ffmpeg = createFFmpeg({ log: true });
+    let logElem = document.getElementById('log');
+    console.log = function (message) {
+        logElem.innerHTML += message + '<br />';
+        logElem.scrollTop = logElem.scrollHeight;
+    };
 
-(async () => {
-    await ffmpeg.load();
-    ffmpeg.FS('writeFile', 'input.mp4', await fetchFile('./cat.mp4'));
-    ffmpeg.FS('writeFile', 'font.ttf', await fetchFile('./OpenSans-Regular.ttf'));
+    const ffmpeg = createFFmpeg({ log: true });
     
-    await ffmpeg.run('-i', 'input.mp4', '-vf',
-    'drawtext= fontfile=font.ttf: text=\'cat\': fontcolor=black: x=200: y=550: fontsize=48, drawtext= fontfile=font.ttf: text=\'drummer\': fontcolor=white: x=850: y=400: fontsize=48, drawtext= fontfile=font.ttf: text=\'drum\': x=650: y=600: fontcolor=white: fontsize=48',
-    '-codec:a', 'copy', 'output.mp4'
-    );
-    
-    const data = ffmpeg.FS('readFile', 'output.mp4');
-    const video = document.getElementById('output-video');
-    video.src = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
-})();
+    document.getElementById('make_meme').onclick = function() {
+        (async () => {
+            if(!ffmpeg.isLoaded()) {
+                await ffmpeg.load();
+            }
+            
+            ffmpeg.FS('writeFile', 'input.mp4', await fetchFile('./cat.mp4'));
+            ffmpeg.FS('writeFile', 'font.ttf', await fetchFile('./OpenSans-Regular.ttf'));
+            
+            await ffmpeg.run('-i', 'input.mp4', '-vf',
+            'drawtext= fontfile=font.ttf: text=\''
+            +document.getElementById("cat").value
+            +'\': fontcolor=black: x=200: y=550: fontsize=48, drawtext= fontfile=font.ttf: text=\''
+            +document.getElementById("drummer").value
+            +'\': fontcolor=white: x=850: y=400: fontsize=48, drawtext= fontfile=font.ttf: text=\''
+            +document.getElementById("drum").value
+            +'\': x=650: y=600: fontcolor=white: fontsize=48',
+            '-codec:a', 'copy', 'output.mp4'
+            );
+            
+            const data = ffmpeg.FS('readFile', 'output.mp4');
+            const video = document.getElementById('output-video');
+            video.src = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
+        })();
+    }
+}
